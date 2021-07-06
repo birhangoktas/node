f559c474771fa4f31b74eef93051c958362301e5
Birhan Göktaş <birhan_goktas55@hotmail.com>
2021-07-07 01:17:05 +0300

    Pub Trivia 1.1

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ app.get('/',(req,res) => {
     res.send('Sonunda');
 });
 
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is listening on: ", port);
+});
